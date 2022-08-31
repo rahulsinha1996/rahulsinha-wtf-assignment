0b473402fb31355c86630b54230f479971d73d77
Rahul Sinha <99676586+rahulsinha1996@users.noreply.github.com>
2022-09-01 02:35:13 +0530

    Update

--- wtf-backend assignment/index.js
+++ wtf-backend assignment/index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json())
 
 app.use("/users", userRoute)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/",(req,res)=>{
     res.send("Server is running.");
